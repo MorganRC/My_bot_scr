ee1f9f2fc8e3f30bea146377319f85ee563c6869
MorganRC <117770054+MorganRC@users.noreply.github.com>
2022-12-08 13:43:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 def collect_data():
     headers = {
         "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15"
+        "User-Agent": "type_your_user-agent"
     }
     offset = 0
     batch_size = 60 
